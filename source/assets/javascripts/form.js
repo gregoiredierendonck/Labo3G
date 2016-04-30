785f06816e12a043e567a57ea65e42cc7ef1a9e8
gregoiredierendonck <bonjour@gregoiredierendonck.fr>
2016-04-30 13:00:01 +0200

    Add js validation to newsletter form

--- source/assets/javascripts/form.js
+++ source/assets/javascripts/form.js
@@ -1,28 +1,28 @@
 $(document).ready(function(){
 
-	function validateEmail($email) {
-		var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
-		return emailReg.test( $email );
+	function validateEmail(email) { 
+		var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		return re.test(email);
 	}
 
-	$('form').on('submit', function(e) {
-		e.preventDefault();
-		var validEntrie = $('#mce-EMAIL').val();
-
-		if(($('#mce-EMAIL').val()) === ''){
-			console.log('vide');
-			// $(this).addClass('error');
-		}
-
-		if(!validateEmail(validEntrie)) {
-			console.log('faux');
-			// $(this).addClass('error');
+	function validate(){
+		// $("#result").text("");
+		var email = $("#mce-EMAIL").val();
+		if (validateEmail(email)) {
+			// $("#result").text(email + " is valid :)");
+			// $("#result").css("color", "green");
+			$('form').removeClass('form--error');
+			$('form').addClass('form--valid');
+			return true;
 		} else {
-			console.log('ok');
-			// $(this).removeClass('error');
-			// $(this).addClass('valid');
-			return true
+			// $("#result").text(email + "is not valid :(");
+			// $("#result").css("color", "red");
+			// console.log('not ok');
+			$('form').addClass('form--error');
+			return false;
 		}
+	}
 
-	});
+	$("form").bind("submit", validate);
+
 });
