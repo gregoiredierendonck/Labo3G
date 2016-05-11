73e6db68cfdb1450443d126c9543c6291b0f5e30
GREGOIRE DIERENDONCK <gdierendonck@siliconsalad.com>
2016-05-11 15:00:27 +0200

    Add Jaco tracking code

--- source/assets/javascripts/analytics.js
+++ source/assets/javascripts/analytics.js
@@ -6,3 +6,24 @@
 
   ga('create', 'UA-69304119-2', 'auto');
   ga('send', 'pageview');
+
+  (function(e, t) {
+      function r(e, t) {
+          e[t] = function() {
+              e.push([ t ].concat(Array.prototype.slice.call(arguments, 0)));
+          };
+      }
+      var n = "JacoRecorder";
+      (function(e, t, i, s) {
+          if (!i.__VERSION) {
+              e[n] = i;
+              var o = [ "init", "identify", "startRecording", "stopRecording", "removeUserTracking", "setUserInfo" ];
+              for (var u = 0; u < o.length; u++) r(i, o[u]);
+              i.__VERSION = .9, i.__INIT_TIME = 1 * new Date;
+              var a = t.createElement("script");
+              a.async = !0, a.setAttribute("crossorigin", "anonymous"), a.src = s;
+              var f = t.getElementsByTagName("head")[0];
+              f.appendChild(a);
+          }
+      })(e, t, e[n] || [], "//recorder-assets.getjaco.com/recorder.js");
+  }).call(window, window, document), window.JacoRecorder.push([ "init", "3b739754-7f37-4a9a-a4ac-efd1839fe2e3", {} ]);
